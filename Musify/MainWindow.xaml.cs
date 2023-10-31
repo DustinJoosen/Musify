cda82c93413521585b0e91be27a3e68faf2f13df
Dustin Joosen <67952109+DustinJoosen@users.noreply.github.com>
2023-10-31 17:35:51 +0100

    Adds a JsonHandler utility class (#1)

--- Musify/MainWindow.xaml.cs
+++ Musify/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
+﻿using Musify.Models;
+using Musify.Utility;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Musify
 {
@@ -23,6 +16,23 @@ namespace Musify
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await JsonHandler.Add<Song>(new()
+            {
+                Id = 4,
+                Title = "Song title",
+                Artist = "Coole artist",
+                Duration = 184,
+                Genre = "Punk",
+                ReleaseDate = DateTime.Now
+            });
+
+            var songs = await JsonHandler.GetAll<Song>();
+
+            int x = 9;
         }
     }
 }
