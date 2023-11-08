7c016af358f2aa799a94e8a7a04b38004c77fe89
Dustin Joosen <dustinjoosen2003@gmail.com>
2023-11-08 13:06:37 +0100

    Update JsonHandler

--- Musify/Utility/JsonHandler.cs
+++ Musify/Utility/JsonHandler.cs
@@ -1,29 +1,48 @@
 ﻿using Musify.Models;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
-using System.Windows.Media;
-using System.Windows.Shapes;
 
 
 namespace Musify.Utility
 {
     public static class JsonHandler
     {
-        // Generic json options.
+        /// <summary>
+        /// Options for writing to the json file.
+        /// </summary>
         private static readonly JsonSerializerOptions _options = new()
         {
             WriteIndented = true,
             ReferenceHandler = ReferenceHandler.IgnoreCycles
         };
 
-        // Gives you all items of given type.
+        /// <summary>
+        /// Returns a specific model
+        /// </summary>
+        /// <typeparam name="T">Type to get</typeparam>
+        /// <param name="id">The Id fitting the model</param>
+        /// <returns>Specified model</returns>
+        public static T GetById<T>(Guid id) where T : IIdentifiable
+        {
+            var items = JsonHandler.GetAll<T>();
+
+            return (from item in items
+                    where item.Id == id
+                    select item).First();
+        }
+
+        /// <summary>
+        /// Returns all models out of file.
+        /// </summary>
+        /// <typeparam name="T">Type to get</typeparam>
+        /// <returns>List of all models</returns>
         public static List<T> GetAll<T>()
         {
             // Determine the file name.
@@ -47,17 +66,12 @@ namespace Musify.Utility
             }
         }
 
-        // Gives by specific ID.
-        public static T GetById<T>(Guid id) where T: IIdentifiable
-        {
-            var items = JsonHandler.GetAll<T>();
-
-            return (from item in items
-                       where item.Id == id
-                       select item).First();
-        }
-
-        // Saves given model.
+        /// <summary>
+        /// Adds the model to the file.
+        /// </summary>
+        /// <typeparam name="T">Type to add the model.</typeparam>
+        /// <param name="model">Model to add</param>
+        /// <returns>Boolean indicating success</returns>
         public static bool Add<T>(T model)
         {
             // Won't save 'nothing'.
@@ -68,11 +82,16 @@ namespace Musify.Utility
             var models = JsonHandler.GetAll<T>();
             models.Add(model);
 
-            return JsonHandler.Save(models);
+            return JsonHandler.SaveAll(models);
         }
 
-        // Overwrites all existing data with a lsit
-        public static bool Save<T>(List<T> models)
+        /// <summary>
+        /// Saves all models. This overwrites the existing data.
+        /// </summary>
+        /// <typeparam name="T">Type to add the models.</typeparam>
+        /// <param name="models">List of models to add</param>
+        /// <returns>Boolean indicating success</returns>
+        public static bool SaveAll<T>(List<T> models)
         {
             if (models == null)
                 return false;
@@ -92,7 +111,81 @@ namespace Musify.Utility
             }
         }
 
-        // Gives you all text content in the specified file.
+        /// <summary>
+        /// Updates a given model
+        /// </summary>
+        /// <typeparam name="T">Type to update the model. Required to implement IIdentifiable</typeparam>
+        /// <param name="id">Id of the model to update.</param>
+        /// <param name="model">Model to update.</param>
+        /// <returns>Boolean indicating success</returns>
+        public static bool Update<T>(Guid id, T model) where T : IIdentifiable
+        {
+            // No updating to something empty.
+            if (model == null)
+                return false;
+
+            // Id's should match
+            if (!id.Equals(model.Id))
+                return false;
+
+            var items = JsonHandler.GetAll<T>();
+
+            for (int i = 0; i < items.Count(); i++)
+            {
+                // if it's no match, continue.
+                if (!items[i].Id.Equals(id))
+                    continue;
+
+                // It's a match. Overwrite with given model.
+                items[i] = model;
+                break;
+            }
+
+            // Update the list to the file.
+            return JsonHandler.SaveAll<T>(items);
+        }
+
+        /// <summary>
+        /// Deletes based on given Id.
+        /// </summary>
+        /// <typeparam name="T">Type to remove model from. Required to implement IIdentifiable</typeparam>
+        /// <param name="id">Id of the model to be deleted.</param>
+        /// <returns>Boolean indicating success</returns>
+        public static bool Delete<T>(Guid id) where T : IIdentifiable
+        {
+            var items = JsonHandler.GetAll<T>();
+
+            for (int i = 0; i < items.Count(); i++)
+            {
+                // if it's no match, continue.
+                if (!items[i].Id.Equals(id))
+                    continue;
+
+                // It's a match. Remove it.
+                items.RemoveAt(i);
+                break;
+            }
+
+            // Update the list to the file.
+            return JsonHandler.SaveAll<T>(items);
+        }
+
+        /// <summary>
+        /// Deletes given model.
+        /// </summary>
+        /// <typeparam name="T">Type to remove model from. Required to implement IIdentifiable</typeparam>
+        /// <param name="model">Model to be deleted.</param>
+        /// <returns>Boolean indicating success</returns>
+        public static bool Delete<T>(T model) where T : IIdentifiable
+        {
+            return JsonHandler.Delete<T>(model.Id);
+        }
+
+        /// <summary>
+        /// Returns all content of the file async.
+        /// </summary>
+        /// <param name="filepath">File to read the content from</param>
+        /// <returns>Content out of the file</returns>
         private static Task<string> GetFileContents(string filepath)
         {
             return Task.Run(async () =>
@@ -110,7 +203,12 @@ namespace Musify.Utility
             });
         }
 
-        // Saves all text content given, in the specified file.
+        /// <summary>
+        /// Saves all given text async.
+        /// </summary>
+        /// <param name="filepath">File to save the content in</param>
+        /// <param name="content">Content to be saved</param>
+        /// <returns>Boolean indicating success</returns>
         private static Task<bool> SaveFileContents(string filepath, string content)
         {
             return Task.Run(async () =>
@@ -130,6 +228,5 @@ namespace Musify.Utility
                 }
             });
         }
-
     }
 }
