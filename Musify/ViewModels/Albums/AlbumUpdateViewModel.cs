82ab78d74f7659e0a2bb8eefe6c41653d6f0629f
Dustin Joosen <67952109+DustinJoosen@users.noreply.github.com>
2023-11-23 17:32:24 +0100

    Album image updating (#13)
    * Image cover updating
    
    * client input validation

--- Musify/ViewModels/Albums/AlbumUpdateViewModel.cs
+++ Musify/ViewModels/Albums/AlbumUpdateViewModel.cs
@@ -1,7 +1,10 @@
-﻿using Musify.Models;
+﻿using Microsoft.Win32;
+using Musify.Models;
 using Musify.Utility;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +13,25 @@ using System.Windows.Input;
 
 namespace Musify.ViewModels.Albums
 {
-    internal class AlbumUpdateViewModel : Album
+    internal class AlbumUpdateViewModel : Album, IDataErrorInfo
     {
         public ICommand OnUpdateAlbum { get; set; }
         public ICommand OnSelectImage { get; set; }
         public ICommand OnCancel { get; set; }
 
+        // Properties
+        private string _imgCoverPreview;
+        public string ImgCoverPreview
+        {
+            get => this._imgCoverPreview;
+            set
+            {
+                this._imgCoverPreview = value;
+                RaisePropertyChanged(nameof(ImgCoverPreview));
+            }
+        }
+
+        private readonly string _originalCoverImagePath;
 
         private Guid _id;
 
@@ -24,6 +40,8 @@ namespace Musify.ViewModels.Albums
         public AlbumUpdateViewModel(Guid id, Action<object> goBack)
         {
             this._id = id;
+
+            this.OnSelectImage = new RelayCommand(SelectImage);
             this.OnUpdateAlbum = new RelayCommand(UpdateAlbum);
             this.OnCancel = new RelayCommand(goBack);
 
@@ -32,11 +50,52 @@ namespace Musify.ViewModels.Albums
             var album = JsonHandler.GetById<Album>(id);
             this.Title = album.Title;
             this.CoverImage = album.CoverImage;
+            this._originalCoverImagePath = album.CoverImage;
             this.ReleaseYear = album.ReleaseYear;
+
+            this.ImgCoverPreview = $"../../Lib/Uploads/{this.CoverImage}";
+        }
+
+        public async void SelectImage(object parameter)
+        {
+            OpenFileDialog dialog = new();
+            dialog.Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg";
+
+            // Select image.
+            if (dialog.ShowDialog() == false)
+            {
+                return;
+            }
+
+            // Display the image in the preview
+            this.ImgCoverPreview = dialog.FileName;
+
+            // Sets filename.
+            this.CoverImage = dialog.FileName;
         }
 
         public void UpdateAlbum(Object obj) 
         {
+            // Updating image to lib.
+            if (this._originalCoverImagePath != this.CoverImage)
+            {
+                try
+                {
+                    // Delete original
+                    File.Delete($"../../../Lib/Uploads/{this._originalCoverImagePath}");
+
+                    // Copies the file down to the local place.
+                    this.CoverImage = Guid.NewGuid().ToString().Replace("-", "") + ".png";
+                    File.Copy(this.ImgCoverPreview, $"../../../Lib/Uploads/{this.CoverImage}");
+                } 
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Couldn't update image");
+                    MessageBox.Show("Couldn't update image");
+                    return;
+                }
+            }
+
             Boolean Success = JsonHandler.Update<Album>(_id, new Album()
             {
                 Id = this._id,
@@ -55,5 +114,26 @@ namespace Musify.ViewModels.Albums
                 MessageBox.Show("Update was not succesful, please try again");
             }
         }
+
+
+        #region DataValidation
+        public string this[string columnName]
+        {
+            get
+            {
+                if (columnName == nameof(Title) && string.IsNullOrEmpty(Title))
+                    return "Please enter a Title";
+
+                if (columnName == nameof(ReleaseYear) && ReleaseYear > DateTime.Today.Year)
+                    return "Release date can't be in the future";
+
+                return string.Empty;
+            }
+        }
+
+        public string Error => string.Empty;
+
+        #endregion
+
     }
 }
